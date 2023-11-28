73f782c09e07286643e00af8039f14e8352d2ba2
German Monroy <germanmonroyrumay@gmail.com>
2023-11-28 11:30:33 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Natillera la abuelita toretto
+        //¿Que necesito para procesar la natillera de 1 cliente?
+        //ENTRADAS
+        int identificadorCliente = 1;
+        long documentoIdentidad = 1035858511567L;
+        byte edadCliente = 34;
+        short cuotaMensualCliente = 25000;
+        double tasaInteresFijo = 0.1;
+        char calificacionCliente = 'a';
+
+    }
+}
